17ecf671e2d266b37ad899864bdca39f55bc6241
Emwat <tawme.noo@gmail.com>
2026-04-06 22:54:29 -0400

    Update characters-1.js on branch

--- characters-1.js
+++ characters-1.js
@@ -146,4 +146,5 @@ export default {
     'Frisk_red': ['The Fallen Human', 16711680, '<:colorless_energy:382625433016991745>'],
     'Ocean_aberrant_colour': ['The Aberrant', 4405835, '<:abby_energy:1471225970080878715>'],
     'Ocean_wielder_colour': ['The Wielder', 16750118, '<:wielder_energy:1475662155159306382>'],
+   	'hedgehog_blue25': ['The Hedgehog', 2324407, '<:colorless_energy:382625433016991745>'],
 };
